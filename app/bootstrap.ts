02859091d86ea11399719a36e4b31965224d944a
tarlepp <tarmo.leppanen@kase.fi>
2016-01-04 21:42:16 +0200

    Moars stuff to bootstrap.

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -1,9 +1,20 @@
+// Angular2 specified stuff
 import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
+
+// 3rd party libraries
+import {AuthHttp} from 'angular2-jwt/angular2-jwt';
+
+// And application itself
 import {AppCmp} from './components/app/app';
 
+// Bootstrap application
 bootstrap(AppCmp, [
-  ROUTER_PROVIDERS,
-  provide(LocationStrategy, { useClass: HashLocationStrategy })
+  ROUTER_PROVIDERS, HTTP_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy }),
+  provide(AuthHttp, { useFactory: () => {
+    return new AuthHttp();
+  }})
 ]);
