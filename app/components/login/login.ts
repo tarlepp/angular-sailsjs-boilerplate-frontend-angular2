e339000e2fefc24797316bb9a070f75cf19699cf
tarlepp <tarmo.leppanen@kase.fi>
2016-01-04 22:39:57 +0200

    Added login component

---
+++ app/components/login/login.ts
@@ -0,0 +1,55 @@
+// Angular2 specified stuff
+import {Component, Injectable} from 'angular2/core';
+import {FormBuilder, Validators, FORM_DIRECTIVES} from 'angular2/common';
+import {Router} from 'angular2/router';
+import {Http, Headers} from 'angular2/http';
+
+// Component setup
+@Component({
+  selector: 'login',
+  templateUrl: './components/login/login.html',
+  styleUrls: ['./components/login/login.css'],
+  directives: [FORM_DIRECTIVES],
+  providers: [FormBuilder]
+})
+
+@Injectable()
+export class LoginCmp {
+  form;
+
+  private apiUrl = '<%= BACKEND_URL %>';
+
+  constructor(
+    fb: FormBuilder,
+    private _http: Http,
+    private _router: Router
+  ) {
+    this.form = fb.group({
+      identifier: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+
+    this._http
+      .post(this.apiUrl + '/login', JSON.stringify(this.form.value), {headers: headers})
+      .subscribe(
+        response => {
+          let data = response.json();
+
+          localStorage.setItem('id_token', data.token);
+          localStorage.setItem('user', JSON.stringify(data.user));
+
+          this._router.navigate(['/Examples/Book']);
+        },
+        error => {
+          console.log('error');
+          console.log(error);
+        }
+      );
+  }
+}
